de4d97b839dc630834a290f230a1002b817d7834
BruAndreo <brunolino2026@gmail.com>
2019-12-31 14:07:52 -0300

    Header pronto

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div class="header-container">
+        <h1 class="logo">facebook</h1>
+
+        <p class="perfil">Meu Perfil <i class="fa fa-user-circle icon"></i></p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
