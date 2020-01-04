75dc046c0e7a3af0239bf83658c78acab75f5a5f
BruAndreo <brunolino2026@gmail.com>
2020-01-03 22:30:57 -0300

    Finalizando estilo do componente de post

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,10 +3,19 @@ import React from 'react';
 function Post({ post }) {
   return (
     <div className="card-post">
-      <img src={post.author.avatar} />
-      <p>{post.author.name}</p>
-      <p>{post.date}</p>
-      <p>{post.content}</p>
+      
+      <span className="perfil">
+        <img src={post.author.avatar} className="imagem-perfil" />
+
+        <span className="nome-data" >
+          <p className="nome-perfil" >{post.author.name}</p>
+          <p className="data-post" >{post.date}</p>
+        </span>
+      </span>
+      
+      <p className="conteudo-post" >{post.content}</p>
+
+      <hr />
     </div>
   );
 }
